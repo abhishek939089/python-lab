d9772a6b2a596cd8f08b3064228776ccb53d7a1e
abhishek sharma <100137322+abhishek939089@users.noreply.github.com>
2022-03-11 22:29:55 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,8 @@
+# WAP IN PYTHON TO CHECK NUMBER IS POSTIVE,NEGATIVE OR ZERO.....
+n = int(input("Enter a number: "))
+if (n==0):
+    print("given number is zero:")
+elif(n<0):
+    print("given number is negative:")
+else:
+    print("given number is positive")
